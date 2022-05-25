7deefc9ab1536ce4b3d8d314ddcc0cf76a735e24
Fotoweck <89674740442@mail.ru>
2022-05-25 14:36:23 +0300

    new

--- Laba_2.py
+++ Laba_2.py
@@ -1,35 +1,38 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[4]:
+# In[13]:
 
 
-from scipy import linalg
-import numpy as np
-
-
-if __name__ == '__main__':
-    # для метода прогонки. 1 над диагональю. И 1 под диагональю
-    u = 1
-    l = 1
-    n = 5
-    m = 5
-    a = [[18, -9, 0, 0, 0],
+from numpy import  zeros
+a = [[18, -9, 0, 0, 0],
          [2, -9, -4, 0, 0],
          [0, -9, 21,-8, 0],
          [0, 0, -4, -10, 5],
          [0, 0, 0, 7, 12]]
-    b = [-81,71, -39, 64, 3]
-    a = np.array(a)
-    b = np.array(b)
-    ab = np.zeros((u + l + 1, m))
-    for j in range(m):
-        for i in range(n):
-            index = u + i - j
-            if 0 <= index < u + l + 1:
-                ab[index][j] = a[i][j]
-    ans = linalg.solve_banded((l, u), ab, b)
-    print(ans)
+b = [-81,71, -39, 64, 3]
+n=5
+x = zeros(n)
+
+#Прямой ход
+m = 1
+for i in range(1,n):
+    m = a[i][i - 1]/a[i-1][i-1] 
+    a[i][i] = a[i][i] - m*a[i-1][i] 
+    b[i] = b[i] - m*b[i-1] 
+
+#Обратный ход
+x[n-1] = b[n-1]/a[n-1][n-1];
+for i in range(n - 2, -1, -1):
+    x[i]=(b[i] - a[i][i + 1]*x[i+1]) / a[i][i]
+
+print(x)
+
+
+# In[ ]:
+
+
+
 
 
 # In[ ]:
